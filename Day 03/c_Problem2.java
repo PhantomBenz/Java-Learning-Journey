06e4eca7ee62a195825fb471c748723048a68285
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-02 16:06:56 +0545

    Day 03 learning

---
+++ Day 03/c_Problem2.java
@@ -0,0 +1,12 @@
+// print if a number is odd or even
+public class c_Problem2 {
+    public static void main(String[] args) {
+        int number = 15;
+
+        if (number % 2 == 0) {
+            System.out.println("Number is even."); // executes if number is divisible by 2
+        } else {
+            System.out.println("Number is odd."); // executes if number is not divisible by 2
+        }
+    }
+}
