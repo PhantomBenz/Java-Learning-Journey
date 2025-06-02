979b68d4f6ec91fe5c9b7abbe684f3e362b3d6e6
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-02 16:05:06 +0545

    Day 03 learning

---
+++ Day 03/b_Problem1.java
@@ -0,0 +1,14 @@
+// Print the larges of 2 integers
+public class b_Problem1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println("The largest number is: " + a); // executes if a is greater than b
+        } 
+        else {
+            System.out.println("The largest numbers is: " + b); // executes if a is not greater than b
+        }
+    }
+}
