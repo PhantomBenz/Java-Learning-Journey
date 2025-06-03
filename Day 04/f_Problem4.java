cf5207ae7d7de87561b69243b2f0ef39810a8a20
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-03 19:34:11 +0545

    Day 4 Learning

---
+++ Day 04/f_Problem4.java
@@ -0,0 +1,16 @@
+// print a pattern of stars
+// ****
+// ****
+// ****
+// ****
+public class f_Problem4 {
+    public static void main(String[] args) {
+        
+        for(int i = 0 ; i < 4; i++){
+
+            System.out.println("****");
+
+        }
+
+    }
+}
