b6a2d59c0fded792defcd59beda45574a86795fa
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-03 19:33:51 +0545

    Day 4 Learning

---
+++ Day 04/e_ForLoop.java
@@ -0,0 +1,12 @@
+public class e_ForLoop {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < 10; ++i) { // Loop from 0 to 9
+            
+            System.out.println("Hello World!!"); 
+            // ...
+            
+        }
+
+    }
+}
