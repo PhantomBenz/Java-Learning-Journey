08923df5c7a6e2584db29288e7af936fe8e58fb0
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-03 19:33:23 +0545

    Day 4 Learning

---
+++ Day 04/b_Problem1.java
@@ -0,0 +1,16 @@
+// print number from 1 to 10
+public class b_Problem1 {
+    public static void main(String[] args) {
+        
+        int counter = 1;
+        
+        while (counter <=10){
+
+            System.out.println(counter);
+
+            counter++;
+
+        }
+
+    }
+}
