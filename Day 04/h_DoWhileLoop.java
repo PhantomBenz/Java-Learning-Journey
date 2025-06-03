2a09df7447aef5b71fcebe6240c7e9589f01f977
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-03 19:34:25 +0545

    Day 4 Learning

---
+++ Day 04/h_DoWhileLoop.java
@@ -0,0 +1,14 @@
+public class h_DoWhileLoop {
+    public static void main(String[] args) {
+        
+        int counter = 0;
+
+        do{
+            System.out.println("Hello World!!");
+            
+            counter++; // Increment counter by 1
+        }while (counter < 10); // Loop runs until counter is less than 10
+
+        // here at least one iteration is guaranteed 
+    }
+}
