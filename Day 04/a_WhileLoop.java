11004aba9b428a8ab05f5e83fc6a771701292345
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-03 19:32:47 +0545

    Day 4 Learning

---
+++ Day 04/a_WhileLoop.java
@@ -0,0 +1,12 @@
+public class a_WhileLoop {
+    public static void main(String[] args) {
+        int counter = 0; // Initialize counter to 0
+        
+        while (counter<10){ // Loop runs until counter is less than 10
+            
+            System.out.println("Hellow World!!");
+
+            counter++; // Increment counter by 1
+        }
+    }
+}
