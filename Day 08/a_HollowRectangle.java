dd87fb31b981067a0dc7b97d31713e603471263b
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-08 19:50:56 +0545

    Day 08 Learning

---
+++ Day 08/a_HollowRectangle.java
@@ -0,0 +1,21 @@
+public class a_HollowRectangle {
+    public static void main(String args[]){
+        Pattern(5);
+    }
+    public static void Pattern(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i == 1 || i == 5 ){
+                    System.out.print("* ");
+                }
+                else if(j == 1 || j == 5){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
