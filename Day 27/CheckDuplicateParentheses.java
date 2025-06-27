6b6f9fca6b955b9422a538ae1b207621d9debc08
PhantomBenz <byanjankarsumoonmct@gmail.com>
2025-06-27 22:35:33 +0545

    Check Duplicate Parentheses

---
+++ Day 27/CheckDuplicateParentheses.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class CheckDuplicateParentheses {
+    public static boolean checkDup(String str) {
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == ')') {
+                int count = 0;
+                while(s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if(count < 1) {
+                    return true;
+                }
+                else {
+                    s.pop();
+                }
+            }
+            else {
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkDup("(a-b)"));
+        System.out.println(checkDup("((a+b))"));
+    }
+}
