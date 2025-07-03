2db5b963d01c124f63c5e6051e7bf84386076fbc
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-03 22:00:43 +0545

    Check Validity of BST

--- Day 33/BinarySearchTree.java
+++ Day 33/BinarySearchTree.java
@@ -122,6 +122,20 @@ public class BinarySearchTree {
         path.remove(path.size()-1);
     }
 
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if(root == null) {
+            return true;
+        }
+        if(min!=null && root.data <= min.data) {
+            return false;
+        }
+        else if(max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
     public static void main(String[] args) {
         // Node root = new Node(4);
         // root.left = new Node(2);
@@ -154,5 +168,7 @@ public class BinarySearchTree {
         System.out.println();
 
         printRoot2Leaf(root, new ArrayList<>());
+
+        System.out.println("Validity of BST : " + isValidBST(root, null, null));
     }    
 }
