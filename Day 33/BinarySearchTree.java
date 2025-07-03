b9ba1b602f9415c49af9d7cd4165259634396120
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-03 21:46:31 +0545

    Path From Root to Leaf

--- Day 33/BinarySearchTree.java
+++ Day 33/BinarySearchTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BinarySearchTree {
     static class Node {
         int data;
@@ -100,6 +102,26 @@ public class BinarySearchTree {
         }
     }
 
+    public static void printPath(ArrayList<Integer> path) {
+        for(int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println("Null");
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if(root == null) {
+            return;
+        }
+        path.add(root.data);
+        if(root.left == null && root.right == null) {
+            printPath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
+
     public static void main(String[] args) {
         // Node root = new Node(4);
         // root.left = new Node(2);
@@ -130,5 +152,7 @@ public class BinarySearchTree {
 
         printInRage(root, 5, 12);
         System.out.println();
+
+        printRoot2Leaf(root, new ArrayList<>());
     }    
 }
