abd13427aa190cae3152d619a4613209460bbecd
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-05 22:14:38 +0545

    Heap Sort

--- Day 35/HeapA.java
+++ Day 35/HeapA.java
@@ -66,8 +66,47 @@ public class HeapA {
         }
     }
 
+    // for descending sort
+
+    public static void heapify(int arr[], int i, int size) {
+        int left = 2*i+1;
+        int right = 2*i+2;
+        int minIdx = i;
+        if(left < size && arr[left] < arr[minIdx]) {
+            minIdx = left;
+        }
+
+        if(right < size && arr[right] < arr[minIdx]) {
+            minIdx = right;
+        }
+
+        if(minIdx != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+
+            heapify(arr, minIdx, size);
+        }
+    }
+
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+        for(int i = n/2; i >= 0; i--) {
+            heapify(arr, i, n);
+        }
+
+        for(int i = n-1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, 0, i);
+        }
+    }
+
     public static void main(String[] args) {
         MinHeap h = new MinHeap();
+
     //     h.add(2);
     //     h.add(3);
     //     h.add(4);
@@ -86,6 +125,16 @@ public class HeapA {
         while(!h.isEmpty()) {
             System.out.print(h.peek() + " ");
             h.remove();
+        }
+        System.out.println();
+
+        // for sorting : Ascending needs maxHeap; Descending needs minHeap
+
+        int arr[] = {5,4,2,3,1,6};        
+        heapSort(arr);
+        System.out.print("Sorted array will be (descending) : ");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
