62df7fe2d4259fb54d0c6b8990032226468d6a9a
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-05 12:53:18 +0545

    Add Basic Heap Operation

---
+++ Day 35/HeapA.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+public class HeapA {
+    static class MinHeap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        private void heapify(int i) {
+            int left = 2*i+1;
+            int right = 2*i+2;
+            int minIdx = i;
+
+            if(left < arr.size() && arr.get(left) < arr.get(minIdx)) {
+                minIdx = left;
+            }
+
+            if(right < arr.size() && arr.get(right) < arr.get(minIdx)) {
+                minIdx = right;
+            }
+
+            if(minIdx != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+        }
+
+        public void add(int data) {
+            arr.add(data);
+
+            int x = arr.size()-1;
+            int par = (x-1)/2;
+
+            while(arr.get(x) < arr.get(par)) {      // O(logn)
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+                x = par;
+                par = (x-1)/2;
+            }
+        }
+
+        public int peek() {
+            return arr.get(0);
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            //step1 - swap first and last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            //step2 - delete last
+            arr.remove(arr.size()-1);
+
+            //step3 - heapify
+            heapify(0);
+
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        MinHeap h = new MinHeap();
+    //     h.add(2);
+    //     h.add(3);
+    //     h.add(4);
+    //     h.add(5);
+    //     h.add(10);
+    //     h.add(1);
+    //     System.out.println(h.peek());
+    //     System.out.println(h.remove());
+    //     System.out.println(h.peek());
+
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while(!h.isEmpty()) {
+            System.out.print(h.peek() + " ");
+            h.remove();
+        }
+        System.out.println();
+    }
+}
